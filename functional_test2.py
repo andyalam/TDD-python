dff4b7cd20f2058d23cd2ea137e42cfb30cdd99e
andyalam <andyalam@hotmail.com>
2016-05-07 20:31:10 -0700

    function test now checks we can input a to-do item

--- functional_test2.py
+++ functional_test2.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,20 +16,34 @@ class NewVisitorTest(unittest.TestCase):
         #The user opens the site
         self.browser.get('http://localhost:8000')
 
-        #Check the page title
+        #Check the page title and the header mentions to-do lists
         self.assertIn('To-Do', self.browser.title)
-
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # The user is invited to enter a to-do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # The user types "Download more RAM" into a text box
+        inputbox.send_keys('Download more RAM')
 
         # When the user hits enter, the page updates, and now
         # the to-do item "Download more RAM" is added to the to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Download more RAM' for row in rows)
+        )
 
         # The user is invited to enter another to-do item via text box
         # The user enters "Download a car"
+        self.fail('Finish the test!')
 
         # The page udpates again and shows the new to-do-item
         # added to the list
