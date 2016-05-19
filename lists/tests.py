b5cf39c9bbceb2fc3280dcafca5496d5ecac15f4
andyalam <andyalam@hotmail.com>
2016-05-19 08:06:57 -0700

    new URL/view/template do display lists. separation from the home page

--- lists/tests.py
+++ lists/tests.py
@@ -42,24 +42,13 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
-
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='First to-do item')
-        Item.objects.create(text='Second to-do item')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('First to-do item', response.content.decode())
-        self.assertIn('Second to-do item', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -80,3 +69,18 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+class LiveViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='First to-do item')
+        Item.objects.create(text='Second to-do item')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'First to-do item')
+        self.assertContains(response, 'Second to-do item')
